6f377efc6fa791aaddf8ea2ec6f1e1f6ec6eb245
5Cord <dima2v@bk.ru>
2024-06-02 17:40:52 +0300

    Отредактировал скролл

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import React, { useRef, useEffect, useState } from 'react';
-import { Routes, Route, Link } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { Routes, Route, useLocation } from 'react-router-dom';
 import Star from './image/Star.svg';
 import RingUp from './image/RingUp.svg';
 import RingDown from './image/RingDown.svg';
@@ -10,23 +10,39 @@ import 'swiper/css/scrollbar';
 import cl from './App.module.scss';
 import { Layout } from './component/Layout';
 import HomePage from './pages/HomePage';
+
 function App() {
+  const location = useLocation();
+
+  useEffect(() => {
+    if (location.pathname === '/page') {
+      document.documentElement.style.overflow = 'scroll';
+      document.documentElement.style.overflowX = 'hidden';
+    } else {
+      document.documentElement.style.overflow = 'hidden';
+    }
+
+    // Очистка эффекта при размонтировании компонента или смене маршрута
+    return () => {
+      document.documentElement.style.overflow = 'auto';
+    };
+  }, [location]);
+
   return (
     <>
-      <div className={cl.baground}>
+      <div className={cl.background}>
         <img className={cl.star} src={Star} alt="" />
         <img className={cl.ringUp} src={RingUp} alt="" />
         <img className={cl.ringDown} src={RingDown} alt="" />
       </div>
       <Routes>
-        <Route>
-          <Route path='/' element={<Layout />}>
-            <Route path='/page' element={<Page />} />
-            <Route index element={<HomePage />} />
-          </Route>
+        <Route path='/' element={<Layout />}>
+          <Route index element={<HomePage />} />
+          <Route path='/page' element={<Page />} />
         </Route>
       </Routes>
     </>
-  )
+  );
 }
-export default App
+
+export default App;
