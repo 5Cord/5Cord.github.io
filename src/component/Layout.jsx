1e3cb1fbccf818158564221112edb234755e7420
5Cord <dima2v@bk.ru>
2024-06-02 15:26:08 +0300

    Рефакторинг кода + роуты

---
+++ src/component/Layout.jsx
@@ -0,0 +1,48 @@
+import React, { useState, useEffect } from 'react';
+import { Link, Outlet } from 'react-router-dom';
+import MyFace from '../image/MyFace.png';
+import cl from '../App.module.scss';
+import MainMenu from '../widgers/MainMenu';
+
+function Layout() {
+    const [showMenu, setShowMenu] = useState(false);
+    const handleMenu = (event) => {
+        event.stopPropagation();
+        setShowMenu((prev) => !prev);
+    };
+    useEffect(() => {
+        const closeMenu = () => {
+            if (showMenu) {
+                setShowMenu(false);
+            }
+        };
+
+        document.addEventListener('click', closeMenu);
+        return () => {
+            document.removeEventListener('click', closeMenu);
+        };
+    }, [showMenu]);
+    return (
+        <>
+            <header>
+            <MainMenu show={showMenu} />
+                <Link to="/test">1</Link>
+                <div className={cl.header}>
+                    <div className={cl.header_logo}>
+                        <img src={MyFace} className={cl.header_logo_img} alt="MyFace" />
+
+                        <div className={cl.header_logo_text}>VD</div>
+                    </div>
+                    <div className={cl.header_menu} onClick={handleMenu}>
+                        <div className={cl.header_menu_pointMenu}></div>
+                        <div className={cl.header_menu_pointMenu}></div>
+                        <div className={cl.header_menu_pointMenu}></div>
+                    </div>
+                </div>
+            </header>
+            <Outlet />
+        </>
+    )
+}
+
+export { Layout }
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,85 @@
+import React, { useRef, useEffect, useState } from 'react';
+import { Routes, Route, Link } from 'react-router-dom';
+
+import Arrow from '../image/Arrow.svg';
+import iconButton from '../image/iconButton.svg';
+import { Scrollbar, A11y } from 'swiper/modules';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/scrollbar';
+import cl from '../App.module.scss';
+const changePage = (swiper, direction, setCount) => {
+    if (swiper) {
+        if (direction === 'next') {
+            swiper.slideNext();
+            setCount((prevCount) => prevCount + 1);
+        } else {
+            swiper.slidePrev();
+            setCount((prevCount) => prevCount - 1);
+        }
+    }
+};
+function HomePage() {
+    const swiperRef = useRef(null);
+    const [countPage, setCountPage] = useState(0);
+    const [showStackProject, setshowStackProject] = useState(false);
+
+
+    const handleCaseClick = () => {
+        changePage(swiperRef.current, 'next', setCountPage);
+    };
+
+    const handleCaseClickPrev = () => {
+        changePage(swiperRef.current, 'prev', setCountPage);
+    };
+
+    return (
+        <>
+            <Swiper
+                modules={[Scrollbar, A11y]}
+                spaceBetween={50}
+                slidesPerView={1}
+                onSwiper={(swiper) => {
+                    swiperRef.current = swiper; // Сохранение ссылки на Swiper
+                }}
+                className={cl.fullPageSwiper}
+            >
+                <SwiperSlide><div className={cl.contrainer_center}>
+                    <div className={`${cl.contrainer_center_up} ${cl.contrainer_center_text}`}>Vaytovich Dmitriy</div>
+                    <div className={`${cl.contrainer_center_down} ${cl.contrainer_center_text}`}>Frontend разработчик</div>
+                </div></SwiperSlide>
+                <SwiperSlide><div className={`${cl.contrainer_center} ${cl.contrainer_center_title}`}>
+                    <div className={`${cl.contrainer_center_up} ${cl.contrainer_center_text}`}>Сайт предприятия</div>
+                    <div className={`${cl.button_viewProject} ${cl.contrainer_center_text}`}>
+                        <button
+                            onMouseEnter={() => setshowStackProject(true)}
+                            onMouseLeave={() => setshowStackProject(false)}>
+                            View Project
+                            <img src={iconButton} alt="" />
+                        </button>
+                    </div>
+                    <div className={showStackProject ? cl.button_viewProject_block_view : cl.button_viewProject_block}>
+                        <div className={cl.button_viewProject_point}>HTML</div>
+                        <div className={cl.button_viewProject_point}>SCSS</div>
+                        <div className={cl.button_viewProject_point}>PHP</div>
+                        <div className={cl.button_viewProject_point}>Mysql</div>
+                        <div className={cl.button_viewProject_point}>JS</div>
+                    </div>
+                </div></SwiperSlide>
+                <SwiperSlide>Slide 3</SwiperSlide>
+                <SwiperSlide>Slide 4</SwiperSlide>
+            </Swiper>
+            <div className={cl.arrowCase} onClick={handleCaseClick}>
+                <div className={cl.case_text}>Кейсы</div>
+                <img src={Arrow} className={cl.case_arrow} alt="Arrow" />
+            </div>
+            {countPage ? (<div className={cl.arrowCasePrev} onClick={handleCaseClickPrev}>
+                <img src={Arrow} className={cl.case_arrowPrev} alt="Arrow" />
+                <div className={cl.case_text}>Назад</div>
+            </div>) : ''}
+
+        </>
+    )
+}
+export default HomePage
