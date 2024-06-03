a42a112a8692630d25e4bb78d4c0e876e870ba3f
5Cord <dima2v@bk.ru>
2024-06-03 12:01:03 +0300

    cteate page about me

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,14 +1,29 @@
 import React from 'react'
 import iconT from '../image/iconT.png';
 import iconG from '../image/iconG.png';
-import Shadow from '../image/shadow.svg';
-import cl from './PageStyle.module.scss';
+import MyFaceBig from '../image/bigFace.png';
+import cl from './AboutStyle.module.scss';
 function About() {
     return (
         <>
             <div className={cl.container}>
-            <h1>Привет! Я молодой</h1>
-            <h1>Frontend developer</h1>
+                <h1>Привет! Я молодой <br></br>
+                    Frontend developer</h1>
+                <div className={cl.container_myFace}><img src={MyFaceBig} alt="" /></div>
+                <div className={cl.container_tecnology}>
+                    <h1 className={cl.h1_indent100}>Стэк технологий</h1>
+                    <div className={cl.container_stack}>
+                        <div className={cl.point_stack}>HTML</div>
+                        <div className={cl.point_stack}>SCSS</div>
+                        <div className={cl.point_stack}>Vite</div>
+                        <div className={cl.point_stack}>BEM</div>
+                        <div className={cl.point_stack}>MySQL</div>
+                        <div className={cl.point_stack}>JS</div>
+                        <div className={cl.point_stack}>React</div>
+                        <div className={cl.point_stack}>Node js</div>
+                        <div className={cl.point_stack}>PHP</div>
+                    </div>
+                </div>
             </div>
             <footer>
                 <div className={cl.footer_text}>Frontend-developer</div>
