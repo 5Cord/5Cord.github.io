b1e61fcc23a8df3d6d036ed0eabb151e7e691aad
5Cord <dima2v@bk.ru>
2024-06-22 11:47:43 +0300

    Added routes to the menu

--- src/widgers/MainMenu.jsx
+++ src/widgers/MainMenu.jsx
@@ -1,15 +1,19 @@
 import React from 'react';
 import cl from './MenuStyle.module.scss';
+import { Route, Routes, Link } from "react-router-dom"
+import { About } from '../pages/AboutPage';
 
 function MainMenu({ show }) { // Получаем пропс show
     return (
-        <div className={`${cl.MainMenu} ${show ? cl.MainMenu_open : ''}`}>
-            <div className={cl.MainMenu_title}>Меню</div>
-            <div className={cl.MainMenu_point}>Обо мне</div>
-            <div className={cl.MainMenu_point}>Контакты</div>
-            <div className={cl.MainMenu_point}>GitHub</div>
-            <div className={cl.MainMenu_point}>Резюме</div>
-        </div>
+        <>
+            <div className={`${cl.MainMenu} ${show ? cl.MainMenu_open : ''}`}>
+                <div className={cl.MainMenu_title}>Меню</div>
+                <div className={cl.MainMenu_point}><Link to="/about">Обо мне</Link></div>
+                <div className={cl.MainMenu_point}>Контакты</div>
+                <div className={cl.MainMenu_point}>GitHub</div>
+                <div className={cl.MainMenu_point}>Резюме</div>
+            </div>
+        </>
     );
 }
 
