65b60d83c55a366fa4222761daed9fa28e1f1135
5Cord <dima2v@bk.ru>
2024-09-27 10:41:53 +0300

    Removed the parallax effect from mobile devices

--- src/useParallax.jsx
+++ src/useParallax.jsx
@@ -1,32 +1,21 @@
 import { useState, useEffect } from 'react';
 
-function useParallax(speed = 0.05) {
+function useMouseParallax(speed = 0.05) {
   const [offset, setOffset] = useState({ x: 0, y: 0 });
 
   useEffect(() => {
     const handleMouseMove = (event) => {
       const { innerWidth, innerHeight } = window;
-      const x = -(event.clientX - innerWidth / 2) * speed;
+      const x = -(event.clientX - innerWidth / 2) * speed; // Инвертируем направление
       const y = -(event.clientY - innerHeight / 2) * speed;
       setOffset({ x, y });
     };
 
-    const handleDeviceOrientation = (event) => {
-      const x = event.gamma * speed; // горизонтальный наклон устройства
-      const y = event.beta * speed;  // вертикальный наклон устройства
-      setOffset({ x, y });
-    };
-
     window.addEventListener('mousemove', handleMouseMove);
-    window.addEventListener('deviceorientation', handleDeviceOrientation);
-
-    return () => {
-      window.removeEventListener('mousemove', handleMouseMove);
-      window.removeEventListener('deviceorientation', handleDeviceOrientation);
-    };
+    return () => window.removeEventListener('mousemove', handleMouseMove);
   }, [speed]);
 
   return offset;
 }
 
-export default useParallax;
+export default useMouseParallax;
