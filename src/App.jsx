9c8bc473d7f1ae8c457b36840113f07349e66da3
5Cord <dima2v@bk.ru>
2024-09-27 11:41:54 +0300

    parallax off

--- src/App.jsx
+++ src/App.jsx
@@ -12,14 +12,8 @@ import cl from './App.module.scss';
 import { Layout } from './component/Layout.jsx';
 import HomePage from './pages/HomePage.jsx';
 import { Contact } from './pages/Contact';
-import useParallax from './useParallax.jsx';
 
 function App() {
-  const isMobile = window.innerWidth < 768; // Определяем, мобильное ли устройство
-  const offset1 = useParallax(isMobile ? 0 : 0.05); // Отключаем параллакс на мобильных
-  const offset2 = useParallax(isMobile ? 0 : 0.03);
-  const offset3 = useParallax(isMobile ? 0 : 0.04);
-  
   const location = useLocation();
 
   useEffect(() => {
@@ -40,31 +34,10 @@ function App() {
   return (
     <>
       <div className={cl.background}>
-        <img
-          className={cl.star}
-          src={Star}
-          alt="Star"
-          style={{
-            transform: `translate(${offset1.x}px, ${offset1.y}px)`,
-          }}
-        />
-        <img
-          className={cl.ringUp}
-          src={RingUp}
-          alt="RingUp"
-          style={{
-            transform: `translate(${offset2.x}px, ${offset2.y}px)`,
-          }}
-        />
+        <img className={cl.star} src={Star} alt="Star" />
+        <img className={cl.ringUp} src={RingUp} alt="RingUp" />
         {!isPageRoute && (
-          <img
-            className={cl.ringDown}
-            src={RingDown}
-            alt="RingDown"
-            style={{
-              transform: `translate(${offset3.x}px, ${offset3.y}px)`,
-            }}
-          />
+          <img className={cl.ringDown} src={RingDown} alt="RingDown" />
         )}
       </div>
       <Routes>
